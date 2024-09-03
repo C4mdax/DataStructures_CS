df403cbb757c40c49ac60b88ab2d8d803e65e9ae
C4mdax <mod.angel48@ciencias.unam.mx>
2024-09-03 17:58:08 -0600

    Agregando practica 2

---
+++ Practices/practica2/src/main/java/mx/unam/ciencias/edd/Pila.java
@@ -0,0 +1,42 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Clase para pilas genéricas.
+ */
+public class Pila<T> extends MeteSaca<T> {
+
+    /**
+     * Regresa una representación en cadena de la pila.
+     * @return una representación en cadena de la pila.
+     */
+    @Override public String toString() {
+	String s = "";
+	Nodo nodo = cabeza;
+
+	while(nodo != null){
+	    s += nodo.elemento.toString() + "\n";
+	    nodo = nodo.siguiente;
+	}
+	return s;   	
+    }
+
+    /**
+     * Agrega un elemento al tope de la pila.
+     * @param elemento el elemento a agregar.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *         <code>null</code>.
+     */
+    @Override public void mete(T elemento) {
+	if (elemento == null)
+            throw new IllegalArgumentException("El elemento no es válido.");
+
+        Nodo nodo = new Nodo(elemento);
+
+        if (cabeza == null)
+            cabeza = rabo = nodo;
+        else {
+            nodo.siguiente = cabeza;
+            cabeza = nodo;
+        }
+    }
+}
