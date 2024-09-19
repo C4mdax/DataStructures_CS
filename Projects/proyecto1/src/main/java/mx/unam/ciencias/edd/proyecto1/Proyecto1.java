c1318db50ed1c0d06617d3cc6b5b9fa9b6819de5
C4mdax <mod.angel48@ciencias.unam.mx>
2024-09-19 10:55:46 -0600

    Version con banderas parcialmente correcta

--- Projects/proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
+++ Projects/proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
@@ -13,31 +13,40 @@ public class Proyecto1 {
         boolean reversa = false; /*"-r": texto ordenado al revés.*/
         boolean escritura = false; /*"-o": texto ordenado sobrescribiendo el archivo dado*/
 	
+	Lista<String> textoEntrada = new Lista<>();
 	Lista<String> entrada = new Lista<>();
 	Procesador procesador = new Procesador();
 
 	if (args.length == 0){
-	    procesador.getEstandar(entrada);
+	    procesador.getEstandar(textoEntrada);
 	}
 	else{
 	    for (String argumento : args){
+		entrada.agrega(argumento);
 		if (argumento.equals("-o")) escritura = true;
 		
 		else if (argumento.equals("-r")) reversa = true;
 		else
-		    procesador.getArgumento(argumento, entrada);
+		    procesador.getArgumento(argumento, textoEntrada);
+	    }
+	}
+	Argumentos banderas = new Argumentos(reversa, escritura);
+	Lista<String> ordenada = Ordenador.ordena(textoEntrada);
+	
+	if (banderas.MODO_ESCRITURA){
+	    int indiceBandera = entrada.indiceDe("-o");
+	    if (indiceBandera == args.length - 1)
+		System.err.println("Uso correcto de bandera -o: java -jar target/proyecto1.jar -o <ruta_archivo>");
+	    else{
+		banderas.escribe(args[indiceBandera+1], ordenada);
+		System.out.println("Se ha sobreescrito el archivo " + args[indiceBandera+1] + " correctamente");
 	    }
 	}
 
-	Argumentos banderas = new Argumentos(reversa, escritura);
-	Lista<String> ordenada = Ordenador.ordena(entrada);
 	if (banderas.MODO_REVERSA){
-	    Lista<String> reversa = banderas.reversa(ordenada);
-	    for (String linea : reversa)
+	    Lista<String> inversa = banderas.reversa(ordenada);
+	    for (String linea : inversa)
 		System.out.println(linea);
 	}
-	else if (banderas.MODO_ESCRITURA)
-	    banderas.escribe()
-	
     }
 }
