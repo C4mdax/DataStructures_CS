1d27b1bc2dd14de54cdb28497b368ba49d2fcbf6
C4mdax <mod.angel48@ciencias.unam.mx>
2024-09-20 17:19:08 -0600

    Banderas correctas excepto -o

--- Projects/proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
+++ Projects/proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
@@ -30,22 +30,30 @@ public class Proyecto1 {
 		    procesador.getArgumento(argumento, textoEntrada);
 	    }
 	}
+	/* Creamos una instancia de la clase Argumentos la cuál toma como parámetros los valores
+	 * booleanos de las respectivas banderas.
+	 */
 	Argumentos banderas = new Argumentos(reversa, escritura);
-	Lista<String> ordenada = Ordenador.ordena(textoEntrada);
 	
-	if (banderas.MODO_ESCRITURA){
-	    int indiceBandera = entrada.indiceDe("-o");
-	    if (indiceBandera == args.length - 1)
-		System.err.println("Uso correcto de bandera -o: java -jar target/proyecto1.jar -o <ruta_archivo>");
-	    else{
-		banderas.escribe(args[indiceBandera+1], ordenada);
-		System.out.println("Se ha sobreescrito el archivo " + args[indiceBandera+1] + " correctamente");
-	    }
-	}
-
+	Lista<String> ordenada = Ordenador.ordena(textoEntrada);
+	/* Se especificó la bandera -r: impresión de la lista en orden inverso.*/
 	if (banderas.MODO_REVERSA){
 	    Lista<String> inversa = banderas.reversa(ordenada);
 	    for (String linea : inversa)
+		System.out.println(linea);
+	}
+	/* Se especificó la bandera -o: se sobrescribirá el archivo dado con el texto ordenado.*/
+	else if (banderas.MODO_ESCRITURA){
+	    int indiceBandera = entrada.indiceDe("-o");
+	    if (indiceBandera == args.length-1)
+		System.err.println("Uso correcto de bandera -o: java -jar target/proyecto1.jar -o <ruta_archivo>");
+	    else{
+		banderas.escribe(args[indiceBandera+1], ordenada);
+		System.out.println("Se ha sobrescrito el archivo correctamente.");
+	    }
+	}
+	else{
+	    for (String linea : ordenada)
 		System.out.println(linea);
 	}
     }
